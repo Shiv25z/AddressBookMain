f1697f72f25994a8a0a2aa9bf05899fad4d8cbf1
Shiv25z <shivamzagade25899@gmail.com>
2022-10-06 15:34:39 +0530

    Added: Delete Option to Remove Person Details

--- com/addressBook/AddressBookMain.java
+++ com/addressBook/AddressBookMain.java
@@ -9,6 +9,7 @@ public class AddressBookMain {
         Contacts.newContact();
 
         Contacts.editContact();
+        Contacts.deleteContact();
     }
 
     static class Contacts {
@@ -53,22 +54,22 @@ public class AddressBookMain {
                     contact.set(2, address);
                     break;
                 case "State":
-                    System.out.println("Enter the name to edit  : ");
+                    System.out.println("Enter New State  : ");
                     String state = sc.nextLine();
                     contact.set(3, state);
                     break;
                 case "ZIP":
-                    System.out.println("Enter the name to edit  : ");
+                    System.out.println("Enter new ZIP : ");
                     String zip = sc.nextLine();
                     contact.set(4, zip);
                     break;
                 case "Phone":
-                    System.out.println("Enter the name to edit  : ");
+                    System.out.println("Enter new Phone number  : ");
                     String phone = sc.nextLine();
                     contact.set(5, phone);
                     break;
-                case "email":
-                    System.out.println("Enter the name to edit  : ");
+                case "Email":
+                    System.out.println("Enter new Email: ");
                     String email = sc.nextLine();
                     contact.set(6, email);
                     break;
@@ -78,6 +79,22 @@ public class AddressBookMain {
 
             }
             System.out.println(contact);
+        }
+
+        static void deleteContact(){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter FirstName to delete contact:");
+            String s = sc.nextLine();
+            if(s.equals(contact.get(0))){
+                contact.removeAll(contact);
+                System.out.println(" ");
+                System.out.println("Contact deleted successfully");
+                System.out.println(contact);
+            }
+            else{
+                System.out.println("Enter Valid FirstName!");
+            }
+
         }
     }
 }
